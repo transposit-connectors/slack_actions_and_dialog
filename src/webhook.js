f09832bee80eb7883c081bcc95f0577d648024ac
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-02 20:27:21 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -10,7 +10,7 @@
     };
   }
   else {
-    api.run('this.post_confirmation', {userid: body.user.id, message: body.message.text, url: `http://example.com/${body.user.name}/clip`, poster: body.message.user, importance: 'High'});
+    api.run('this.post_confirmation', {userid: body.user.id, message: body.message.text, url: `http://example.com/${body.user.name}/clip`, poster: api.run('this.id_to_name',{id: body.message.user}, importance: 'High'});
   }
   return body; //{ status_code: 200 };
 }
