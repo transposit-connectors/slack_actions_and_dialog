2ce9a3847ff1e2ebdc6d42ad0132665dcb619af1
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-08 23:29:17 +0000

    Commit from Transposit IDE

--- src/clipit_confirmation.js
+++ src/clipit_confirmation.js
@@ -1,11 +1,11 @@
 ({ http_event }) => {
   let qs = require('qs.js');
   let body = JSON.parse(qs.parse(http_event.body).payload);
-  //if (body.message) {
-  	api.run('this.post_confirmation', {userid: body.user.id, message: body.message.text, url: `http://example.com/${body.user.name}/clip`, poster: api.run('this.id_to_name',{id: body.message.user})[0].name, importance: 'High'});
-  //}
-  //else {
-  //  api.run('this.open_dialog', {trigger_id: body.trigger_id, message: body.message.text, name: api.run('this.id_to_name',{id: body.message.user})[0].name});
-  //}
+  if (body.message) {
+    api.run('this.open_dialog', {trigger_id: body.trigger_id, message: body.message.text, name: api.run('this.id_to_name',{id: body.message.user})[0].name});
+  }
+  else {
+    api.run('this.post_confirmation', {userid: body.user.id, message: body.message.text, url: `http://example.com/${body.user.name}/clip`, poster: api.run('this.id_to_name',{id: body.message.user})[0].name, importance: 'High'});
+  }
   return { status_code: 200 };
 }
