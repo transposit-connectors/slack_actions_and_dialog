df5b44e52f6c07b9993f15175417231485608e6f
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-08 23:25:11 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -1,6 +1,6 @@
 ({ http_event }) => {
   let qs = require('qs.js');
   let body = JSON.parse(qs.parse(http_event.body).payload);
-  api.run('this.open_dialog', {token: body.trigger_id, message: body.message.text, name: api.run('this.id_to_name',{id: body.message.user})[0].name});
+  api.run('this.open_dialog', {trigger_id: body.trigger_id, message: body.message.text, name: api.run('this.id_to_name',{id: body.message.user})[0].name});
   return { status_code: 200 };
 }
