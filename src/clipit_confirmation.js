933e372eb05bd2a2515db78c715aa97541165b53
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-08 17:33:30 +0000

    Commit from Transposit IDE

--- src/clipit_confirmation.js
+++ src/clipit_confirmation.js
@@ -2,5 +2,5 @@
   let qs = require('qs.js');
   let body = JSON.parse(qs.parse(http_event.body).payload);
   api.run('this.post_confirmation', {userid: body.user.id, message: body.message.text, url: `http://example.com/${body.user.name}/clip`, poster: api.run('this.id_to_name',{id: body.message.user})[0].name, importance: 'High'});
-  return { status_code: 200 };
+  return body;//{ status_code: 200 };
 }
--- src/webhook.js
+++ src/webhook.js
@@ -1,20 +1,16 @@
 ({ http_event }) => {
-  let qs = require('qs.js');
-  let body = JSON.parse(qs.parse(http_event.body).payload);
-  api.run('this.post_confirmation', {userid: body.user.id, message: body.message.text, url: `http://example.com/${body.user.name}/clip`, poster: api.run('this.id_to_name',{id: body.message.user})[0].name, importance: 'High'});
- 
-	// let qs = require('qs.js');
-	// let body = JSON.parse(qs.parse(http_event.body).payload);
-	// if (body.challenge) {
-	// // https://api.slack.com/events/url_verification
-	// return {
-	// status_code: 200,
-	// headers: { "Content-Type": "text/plain" },
-	// body: body.challenge
-	// };
-	// }
-	// else {
-	// api.run('this.open_dialog', {token: body.trigger_id, message: body.message.text, name: api.run('this.id_to_name',{id: body.message.user})[0].name});
+	let qs = require('qs.js');
+	let body = JSON.parse(qs.parse(http_event.body).payload);
+	if (body.challenge) {
+	// https://api.slack.com/events/url_verification
+	return {
+	status_code: 200,
+	headers: { "Content-Type": "text/plain" },
+	body: body.challenge
+	};
+	}
+	else {
+	api.run('this.open_dialog', {token: body.trigger_id, message: body.message.text, name: api.run('this.id_to_name',{id: body.message.user})[0].name});
 	// }
   return { status_code: 200 };
 }
