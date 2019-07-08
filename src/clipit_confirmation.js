f68bb1fde1317cbd8499867ea89da68a4d1fd1de
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-08 23:40:38 +0000

    Commit from Transposit IDE

--- src/clipit_confirmation.js
+++ src/clipit_confirmation.js
@@ -5,7 +5,7 @@
     api.run('this.open_dialog', {trigger_id: body.trigger_id, message: body.message.text, name: api.run('this.id_to_name',{user: body.message.user})[0].name});
   }
   else {
-    api.run('this.post_confirmation', {userid: body.user.id, message: body.submission.text, url: `http://example.com/${body.user.name}/clip`, poster: body.user.name, importance: body.submission.importance});
+    api.run('this.post_confirmation', {userid: body.user.id, message: body.submission.message, url: `http://example.com/${body.user.name}/clip`, poster: body.user.name, importance: body.submission.importance});
   }
   return { status_code: 200 };
 }
