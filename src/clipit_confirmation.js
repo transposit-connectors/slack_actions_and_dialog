390772f95809fe8c04db221e41540c1acea1ca53
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-08 23:36:27 +0000

    Commit from Transposit IDE

--- src/clipit_confirmation.js
+++ src/clipit_confirmation.js
@@ -2,7 +2,7 @@
   let qs = require('qs.js');
   let body = JSON.parse(qs.parse(http_event.body).payload);
   if (body.message) {
-    api.run('this.open_dialog', {trigger_id: body.trigger_id, message: body.message.text, name: api.run('this.id_to_name',{id: body.message.user})[0].name});
+    api.run('this.open_dialog', {trigger_id: body.trigger_id, message: body.message.text, name: api.run('this.id_to_name',{id: body.message.user}).name});
   }
   else {
     api.run('this.post_confirmation', {userid: body.user.id, message: body.submission.text, url: `http://example.com/${body.user.name}/clip`, poster: body.user.name, importance: body.submission.importance});
