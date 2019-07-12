7f1aadb22fba74839783a61d0b6c02e22913aa67
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-12 18:30:21 +0000

    Commit from Transposit IDE

--- src/clipit_confirmation.js
+++ src/clipit_confirmation.js
@@ -5,7 +5,10 @@
     api.run('this.open_dialog', {trigger_id: body.trigger_id, message: body.message.text, name: api.run('this.id_to_name',{user: body.message.user})[0].name});
   }
   else {
-    api.run('this.post_confirmation', {userid: body.user.id, message: body.submission.message, url: `http://example.com/${body.user.name}/clip`, poster: body.user.name, importance: body.submission.importance});
+    //Find the entry to set the url
+    let entry = api.run('this.get_records', {id: body.user.id}).pop().id;
+    let url = `https://airtable.com/tbljGpXAuVv4AhDwT/viwbw9dJQtaUQdswj/${entry}?blocks=hide`;
+    api.run('this.post_confirmation', {userid: body.user.id, message: body.submission.message, url: url, poster: body.user.name, importance: body.submission.importance});
   }
   return { status_code: 200 };
 }
