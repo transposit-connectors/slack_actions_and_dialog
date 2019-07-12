bb267ea34165326857544df43ca6ee84e774a6e7
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-12 20:05:50 +0000

    Commit from Transposit IDE

--- src/clipit_confirmation.js
+++ src/clipit_confirmation.js
@@ -5,6 +5,7 @@
     api.run('this.open_dialog', {trigger_id: body.trigger_id, message: body.message.text, name: api.run('this.id_to_name',{user: body.message.user})[0].name});
   }
   else {
+    api.run('this.create_record', {message: body.submission.message, poster: body.user.name, importance: body.submission.importance})
     //Find the entry to set the url
     let entry = api.run('this.get_records').pop().id;
     let url = `https://airtable.com/tbljGpXAuVv4AhDwT/viwbw9dJQtaUQdswj/${entry}?blocks=hide`;
