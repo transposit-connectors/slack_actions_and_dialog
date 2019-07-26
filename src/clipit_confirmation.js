e862fdfaa5fc7a17a8b6723e01277a5320c12041
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-26 17:09:44 +0000

    Commit from Transposit IDE

--- src/clipit_confirmation.js
+++ src/clipit_confirmation.js
@@ -5,7 +5,7 @@
     api.run('this.open_dialog', {trigger_id: body.trigger_id, message: body.message.text, name: api.run('this.id_to_name',{user: body.message.user})[0].name});
   }
   else {
-    api.run('this.create_record', {baseId: env.get("baseId"), message: body.submission.message, poster: body.submission.send_by, importance: body.submission.importance})
+    //api.run('this.create_record', {baseId: env.get("baseId"), message: body.submission.message, poster: body.submission.send_by, importance: body.submission.importance})
     // Automatically set to an example url. If you want to change it, go to environment variables.
     let url = env.get("url");
     api.run('this.post_confirmation', {userid: body.user.id, message: body.submission.message, url: url, poster: body.submission.send_by, importance: body.submission.importance});
