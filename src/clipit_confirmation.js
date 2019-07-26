26eeb1f73309e4a2a1db966748e27958b2455954
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-26 17:36:17 +0000

    Commit from Transposit IDE

--- src/clipit_confirmation.js
+++ src/clipit_confirmation.js
@@ -1,20 +1,19 @@
 ({ http_event }) => {
-  let body = http_event.parsed_body;//JSON.parse(http_event.parsed_body.payload);
-  return body;
-  // if (body.message) {
-  //   api.run('this.open_dialog', {trigger_id: body.trigger_id,
-  //                                message: body.message.text,
-  //                                name: api.run('this.id_to_name',{user: body.message.user})[0].name});
-  // } else {
-  //   api.run('this.create_record', {baseId: env.get("baseId"),
-  //                                  message: body.submission.message,
-  //                                  poster: body.submission.send_by,
-  //                                  importance: body.submission.importance})
-  //   let url = env.get("url");	// Automatically set to an example url. If you want to change it, go to environment variables.
-  //   api.run('this.post_confirmation', {userid: body.user.id,
-  //                                      message: body.submission.message,
-  //                                      url: url, poster: body.submission.send_by,
-  //                                      importance: body.submission.importance});
-  // }
-  // return { status_code: 200 };
+  let body = JSON.parse(http_event.parsed_body.payload);
+  if (body.message) {
+    api.run('this.open_dialog', {trigger_id: body.trigger_id,
+                                 message: body.message.text,
+                                 name: api.run('this.id_to_name',{user: body.message.user})[0].name});
+  } else {
+    api.run('this.create_record', {baseId: env.get("baseId"),
+                                   message: body.submission.message,
+                                   poster: body.submission.send_by,
+                                   importance: body.submission.importance})
+    let url = env.get("url");	// Automatically set to an example url. If you want to change it, go to environment variables.
+    api.run('this.post_confirmation', {userid: body.user.id,
+                                       message: body.submission.message,
+                                       url: url, poster: body.submission.send_by,
+                                       importance: body.submission.importance});
+  }
+  return { status_code: 200 };
 }
