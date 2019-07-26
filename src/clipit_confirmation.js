868289d2c06c1aeaf8a9d3b496aa0f3b9db0e78c
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-26 17:05:57 +0000

    Commit from Transposit IDE

--- src/clipit_confirmation.js
+++ src/clipit_confirmation.js
@@ -5,11 +5,9 @@
     api.run('this.open_dialog', {trigger_id: body.trigger_id, message: body.message.text, name: api.run('this.id_to_name',{user: body.message.user})[0].name});
   }
   else {
-    api.run('this.create_record', {message: body.submission.message, poster: body.submission.send_by, importance: body.submission.importance})
-    //Find the entry to set the url
-    let entry = api.run('this.get_records')[0].id;
-    // Change this url to the path of your own airtable location
-    let url = `https://airtable.com/tbljGpXAuVv4AhDwT/viwbw9dJQtaUQdswj/${entry}?blocks=hide`;
+    api.run('this.create_record', {baseId: env.get("baseId"), message: body.submission.message, poster: body.submission.send_by, importance: body.submission.importance})
+    // Automatically set to an example url. If you want to change it, go to environment variables.
+    let url = env.get("url");
     api.run('this.post_confirmation', {userid: body.user.id, message: body.submission.message, url: url, poster: body.submission.send_by, importance: body.submission.importance});
   }
   return { status_code: 200 };
